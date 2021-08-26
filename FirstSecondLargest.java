820347e236f0c2d9b5455addc3647bf06a1cb134
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-26 12:02:52 +0530

    FirstSecondLargest

---
+++ FirstSecondLargest.java
@@ -0,0 +1,23 @@
+package week3.Assignment;
+
+import java.util.Arrays;
+
+public class FirstSecondLargest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Here is the input
+		int[] data = { 3, 2, 11, 4, 6, 7 };
+
+		/*
+		 * Pseudo Code: 1) Arrange the array in ascending order 2) Pick the 2nd element
+		 * from the last and print it
+		 */
+		Arrays.sort(data);
+	//int d=	data.length;
+	
+		
+		System.out.println(data[data.length-2]);
+	}
+
+}
