379fe3e23b957d80fbce23d503cfd4839618e288
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-26 19:32:48 +0530

    DuplicatesinArrayList

--- DuplicatesinArray.java
+++ DuplicatesinArray.java
@@ -1,48 +1,65 @@
 package week3.Assignment;
 
-public class DuplicatesinArray {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicatesinArrayList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int[] arr = { 14, 12, 13, 11, 15, 14, 18, 16, 17, 19, 18, 17, 20 };
 
+			List<Integer> arr1=new ArrayList<Integer>();
+			
 		// get the length of the array
 		
-		int a=arr.length;
-		// declare an int variable named count
 		
+		// declare an int variable named count
 		int count;
-
-		// iterate from 0 to the array length-1 (outer loop starts here)
-		for(int i=0;i<a-1;i++)
+		for(int i:arr)
 		{
-			count=0;// assign 0 to count
+			arr1.add(i);
+		}
+		
+		System.out.println(arr1);
+		
+		Set<Integer> setarr=new HashSet<Integer>();
+		
+		for(int i=0;i<arr1.size();i++)
+		{
+			//count=0;// assign 0 to count
 			// iterate from i to the length of the array by adding 1 to it (inner loop
 			// starts here)
-			for (int j = i + 1; j < a; j++)
+			for (int k = i + 1;k < arr1.size();k++)
 			{
 				// compare both the loop variables & check they're equal
-				if(arr[i]==arr[j])
-					count++;// increase the count if both the arrays are equal
-			}
-			// Out of the inner loop, check and print the first array variable if count is
-			// more than 0
-			if(count>0)
-			{
-			System.out.println(arr[i]);
+				if(arr1.get(i)==arr1.get(k))
+				{
+					
+					setarr.add(arr1.get(i));
+					//System.out.println(setarr);
+				}
+			
+				
 			}
 		}
-
+		System.out.println(setarr);
+		arr1.removeAll(setarr);
 		
-
+		System.out.println(arr1);
 		
-
-		
-		
-
-		
-
 	}
-
 }
+
+
+		
+		
+
+		
+
+	
+
