cc2efcb66211d18128265cb9df1ed57adb0bfa89
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-26 13:44:06 +0530

    FirstSecondLargestArrayList

--- FirstSecondLargest.java
+++ FirstSecondLargest.java
@@ -1,6 +1,9 @@
 package week3.Assignment;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class FirstSecondLargest {
 
@@ -8,15 +11,24 @@ public class FirstSecondLargest {
 		// TODO Auto-generated method stub
 		// Here is the input
 		int[] data = { 3, 2, 11, 4, 6, 7 };
+		
+		Arrays.sort(data);
+		
+		List<Integer> datalist=new ArrayList<Integer>();
+	for(int i:data)
+	{
+		datalist.add(i);
+	}
+	
+	Collections.sort(datalist);
+	System.out.println(datalist);
 
 		/*
 		 * Pseudo Code: 1) Arrange the array in ascending order 2) Pick the 2nd element
 		 * from the last and print it
 		 */
-		Arrays.sort(data);
-	//int d=	data.length;
-	
 		
+	
 		System.out.println(data[data.length-2]);
 	}
 
